7b73121db1460b7d4e158b3bfd3584a8b64850a8
Victor Neves <info@victorneves.dev>
2026-03-13 10:36:05 +0000

    feat: enhance stylelint configuration and update format script to ignore coverage directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -146,7 +146,7 @@ export default [
         files: ['**/*.{ts, js, mjs, vue, json, jsonc}'],
     },
     {
-        ignores: ['**/*.d.ts', 'styleDictionary/build/', 'styleDictionary/config.js'],
+        ignores: ['**/*.d.ts', 'styleDictionary/build/', 'styleDictionary/config.js', 'coverage/**'],
     },
     js.configs.recommended,
     ...tseslint.configs.recommended,
